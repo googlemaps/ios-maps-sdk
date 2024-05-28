0cd72dd45109b0225dd920ff17e0362a96489ec0
Dogahe <dogahe@users.noreply.github.com>
2024-05-28 14:05:34 -0400

    feat: version 9.0.0 (#23)
    * feat: 9.0.0
    
    * chore: update target names to prevent potential conflicts.
    
    * chore: fix target name

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version: 5.5
 
 //
-// Copyright 2023 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,47 +19,40 @@
 import PackageDescription
 
 let package = Package(
-  name: "GoogleMaps", platforms: [.iOS(.v14)],
-  products: [
-    .library(name: "GoogleMaps", targets: ["GoogleMapsTarget"]),
-    .library(name: "GoogleMapsCore", targets: ["GoogleMapsCoreTarget"]),
-    .library(name: "GoogleMapsBase", targets: ["GoogleMapsBaseTarget"]),
-    .library(name: "GoogleMapsM4B", targets: ["GoogleMapsM4BTarget"]),
-  ], dependencies: [],
+  name: "GoogleMaps", platforms: [.iOS(.v15)],
+  products: [.library(name: "GoogleMaps", targets: ["GoogleMapsTarget"])], dependencies: [],
   targets: [
     .binaryTarget(
-      name: "GoogleMaps", url: "https://dl.google.com/geosdk/swiftpm/8.4.0/GoogleMaps_3p.xcframework.zip",
-      checksum: "65f78e9e4e75be666f26584836791fe5685a43b19d847fe5246d4aeb7146d113"
+      name: "GoogleMaps",
+      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMaps_3p.xcframework.zip",
+      checksum: "997a67052607339e379a46d281ee4d198c6448f59176ff03e0a5da5bb315cd14"
     ),
     .target(
       name: "GoogleMapsTarget",
-      dependencies: ["GoogleMaps"],
+      dependencies: [
+        "GoogleMaps",
+        "GoogleMapsBaseTarget",
+        "GoogleMapsCoreTarget",
+      ],
       path: "Maps",
       sources: ["GMSEmpty.m"],
       resources: [.copy("Resources/GoogleMapsResources/GoogleMaps.bundle")],
       publicHeadersPath: "Sources",
       linkerSettings: [
-        .linkedLibrary("z"),
-        .linkedLibrary("c++"),
         .linkedFramework("Accelerate"),
-        .linkedFramework("CoreData"),
-        .linkedFramework("CoreGraphics"),
         .linkedFramework("CoreImage"),
-        .linkedFramework("CoreLocation"),
-        .linkedFramework("CoreTelephony"),
         .linkedFramework("CoreText"),
         .linkedFramework("GLKit"),
         .linkedFramework("ImageIO"),
         .linkedFramework("Metal"),
         .linkedFramework("OpenGLES"),
         .linkedFramework("QuartzCore"),
-        .linkedFramework("SystemConfiguration"),
-        .linkedFramework("UIKit"),
       ]
     ),
     .binaryTarget(
-      name: "GoogleMapsCore", url: "https://dl.google.com/geosdk/swiftpm/8.4.0/GoogleMapsCore_3p.xcframework.zip",
-      checksum: "c7879ccd3cb20d37816cfbb833adb4965d3fd32253f57b1cf2edc0542611ce70"
+      name: "GoogleMapsCore",
+      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMapsCore_3p.xcframework.zip",
+      checksum: "290f4002db479331d18fb9233e3015a1eb0d9abc16a765648e60590f34b0d691"
     ),
     .target(
       name: "GoogleMapsCoreTarget",
@@ -69,26 +62,28 @@ let package = Package(
       publicHeadersPath: "Sources"
     ),
     .binaryTarget(
-      name: "GoogleMapsBase", url: "https://dl.google.com/geosdk/swiftpm/8.4.0/GoogleMapsBase_3p.xcframework.zip",
-      checksum: "d308fb16e60c6d25b221d485fc1695badceaf8c75069179c75c05614a47cc052"
+      name: "GoogleMapsBase",
+      url: "https://dl.google.com/geosdk/swiftpm/9.0.0/GoogleMapsBase_3p.xcframework.zip",
+      checksum: "de00cb1d8557ee90a2fa7dd73060d74ddaf28d6d137082d6df147535b5eb4a01"
     ),
     .target(
       name: "GoogleMapsBaseTarget",
       dependencies: ["GoogleMapsBase"],
       path: "Base",
       sources: ["GMSEmpty.m"],
-      publicHeadersPath: "Sources"
-    ),
-    .binaryTarget(
-      name: "GoogleMapsM4B", url: "https://dl.google.com/geosdk/swiftpm/8.4.0/GoogleMapsM4B_3p.xcframework.zip",
-      checksum: "23665f11fdd498a31fb081d914179e19f2eebdd0366e492f077381748cac21be"
-    ),
-    .target(
-      name: "GoogleMapsM4BTarget",
-      dependencies: ["GoogleMapsM4B"],
-      path: "M4B",
-      sources: ["GMSEmpty.m"],
-      publicHeadersPath: "Sources"
+      publicHeadersPath: "Sources",
+      linkerSettings: [
+        .linkedLibrary("c++"),
+        .linkedLibrary("z"),
+        .linkedFramework("Contacts"),
+        .linkedFramework("CoreData"),
+        .linkedFramework("CoreGraphics"),
+        .linkedFramework("CoreLocation"),
+        .linkedFramework("CoreTelephony"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("UIKit"),
+      ]
     ),
   ]
 )
